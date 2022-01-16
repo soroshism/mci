1bdd20399d5cc30cd42948b8e23014a3666715e0
soroush sohrabi <63061038+soroshism@users.noreply.github.com>
2022-01-16 15:56:36 +0330

    Initial commit

---
+++ assets/php/form-process.php
@@ -0,0 +1,67 @@
+﻿<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "لطفا نام خود را وارد کنید ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "لطفا آدرس ایمیل خود را وارد کنید ";
+} else {
+    $email = $_POST["email"];
+}
+
+// MSG SUBJECT
+if (empty($_POST["msg_subject"])) {
+    $errorMSG .= "موضوع خود را وارد کنید ";
+} else {
+    $msg_subject = $_POST["msg_subject"];
+}
+
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "پیام خود را بنویسید ";
+} else {
+    $message = $_POST["message"];
+}
+
+//Add your email here
+$EmailTo = "soroshism@gmail.com";
+$Subject = "پیام جدید دریافت شد";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $msg_subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
